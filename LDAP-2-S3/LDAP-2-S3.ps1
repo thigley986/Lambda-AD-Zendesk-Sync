95d96acfa684b6747e8d9722defdf9000dc4988f
Taylor Higley <Taylor.Higley@afge.org>
2016-01-01 17:53:17 -0500

    Update LDAP-2-S3.ps1

--- LDAP-2-S3/LDAP-2-S3.ps1
+++ LDAP-2-S3/LDAP-2-S3.ps1
@@ -1,2 +1,3 @@
 #Sync from LDAP to S3 Bucket Engine
-Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item c:\zendesk_sync_tools\active_users.json -type file -force
+$DomainName = Get-ADDomain | Select -ExpandProperty Name
+Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item c:\zendesk_sync\active_users\$DomainName.json -type file -force
