23a81ac802916364f34016e8dc48773ec5a3371f
Taylor Higley <Taylor.Higley@afge.org>
2016-01-01 17:43:00 -0500

    Filter for All Active Users

--- LDAP-2-S3/LDAP-2-S3.ps1
+++ LDAP-2-S3/LDAP-2-S3.ps1
@@ -1,1 +1,2 @@
-#Placeholder file for sync from LDAP to S3 Bucket Engine
+#Sync from LDAP to S3 Bucket Engine
+Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item c:\zendesk_sync_tools\active_users.json -type file -force
