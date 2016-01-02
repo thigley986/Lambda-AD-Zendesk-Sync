195a9252de3f6c33617a220c9fa6e0ef8d65eaf0
Taylor Higley <Taylor.Higley@afge.org>
2016-01-02 14:00:56 -0500

    Update and rename LDAP-2-S3.ps1 to AD-2-S3.ps1

---
+++ LDAP-2-S3/AD-2-S3.ps1
@@ -0,0 +1,22 @@
+#Sync from Active Directory to S3 Bucket Engine
+$accesskey = "AWS Access Key"
+$secretkey = "AWS Secret Key"
+$bucket = "AWS Bucket Name"
+
+Set-AWSCredentials -AccessKey $accesskey -SecretKey $secretkey
+
+try
+    {
+        Import-Module ActiveDirectory
+        $DomainName = Get-ADDomain | Select -ExpandProperty Name
+        echo $env:temp\zendesk_sync\active_users
+        New-Item $env:temp\zendesk_sync\active_users -type directory -force
+        Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item $env:temp\zendesk_sync\active_users\$DomainName.json -type file -force
+
+        echo "Writing to Amazon S3 Bucket"
+        Write-S3Object -BucketName $bucket -Key "$DomainName.json" -File $env:temp\zendesk_sync\active_users\$DomainName.json
+    }
+        catch [Exception]
+    {
+        echo $_.Exception.GetType().FullName, $_.Exception.Message
+    }
