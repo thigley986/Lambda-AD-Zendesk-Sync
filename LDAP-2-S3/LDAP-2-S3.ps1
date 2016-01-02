3dad44bf80ec97a9ea91e34387dd833cdbb19454
Taylor Higley <Taylor.Higley@afge.org>
2016-01-02 12:51:39 -0500

    Module and Exception Handling
    Added Import-Module for Active Directory
    Added exception handling

--- LDAP-2-S3/LDAP-2-S3.ps1
+++ LDAP-2-S3/LDAP-2-S3.ps1
@@ -1,3 +1,11 @@
 #Sync from LDAP to S3 Bucket Engine
-$DomainName = Get-ADDomain | Select -ExpandProperty Name
-Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item c:\zendesk_sync\active_users\$DomainName.json -type file -force
+try
+    {
+        Import-Module ActiveDirectory
+        $DomainName = Get-ADDomain | Select -ExpandProperty Name
+        Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item c:\zendesk_sync\active_users\$DomainName.json -type file -force
+    }
+        catch [Exception]
+    {
+        echo $_.Exception.GetType().FullName, $_.Exception.Message
+    }
