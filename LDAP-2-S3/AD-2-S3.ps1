c9bb3bbcf5df369ff16e6971d81b7d5e503bd4f9
Taylor Higley <Taylor.Higley@afge.org>
2016-01-03 17:07:46 -0500

    Added Filter and Import
    Added filter to remove users without the EmailAddress field (Zendesk cannot create users without an email)
    Importing the AWS Tools to Powershell.  Using an absolute path to support lower versions of PS.   PS 4.0 and above should be able to use "Import-Module AWSPowerShell".
    
    In PowerShell 4.0 and later releases, Import-Module also searches the Program Files folder for installed modules, so it is not necessary to provide the full path to the module. You can run the following command to import the AWSPowerShell module. In PowerShell 3.0 and later, running a cmdlet in the module also automatically imports a module into your session.

--- LDAP-2-S3/AD-2-S3.ps1
+++ LDAP-2-S3/AD-2-S3.ps1
@@ -1,4 +1,6 @@
 #Sync from Active Directory to S3 Bucket Engine
+Import-Module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell\AWSPowerShell.psd1"
+
 $accesskey = "AWS Access Key"
 $secretkey = "AWS Secret Key"
 $bucket = "AWS Bucket Name"
@@ -11,7 +13,7 @@ try
         $DomainName = Get-ADDomain | Select -ExpandProperty Name
         echo $env:temp\zendesk_sync\active_users
         New-Item $env:temp\zendesk_sync\active_users -type directory -force
-        Get-ADUser -Filter {Enabled -eq $true} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item $env:temp\zendesk_sync\active_users\$DomainName.json -type file -force
+        Get-ADUser -Filter {(Enabled -eq $true) -and (EmailAddress -like "*" )} -properties DisplayName, EmailAddress, OfficePhone| select DisplayName, EmailAddress, OfficePhone | ConvertTo-Json | New-Item $env:temp\zendesk_sync\active_users\$DomainName.json -type file -force
 
         echo "Writing to Amazon S3 Bucket"
         Write-S3Object -BucketName $bucket -Key "$DomainName.json" -File $env:temp\zendesk_sync\active_users\$DomainName.json
